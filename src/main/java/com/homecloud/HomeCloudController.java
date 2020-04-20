794da81381e96bdfb8739dd6fa24dc03e5fcef39
Krzysiek <klopr@interia.pl>
2020-04-20 22:13:17 +0200

    Added html with js first logic

--- src/main/java/com/homecloud/HomeCloudController.java
+++ src/main/java/com/homecloud/HomeCloudController.java
@@ -1,24 +1,27 @@
 package com.homecloud;
 
 import com.homecloud.files.FilesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@RestController
+@Controller
 public class HomeCloudController {
 
-    private final FilesService fs;
+    @Autowired
+    private FilesService fs;
 
-    public HomeCloudController(FilesService filesService) {
-        this.fs = filesService;
-    }
+    @GetMapping("/")
+    String index() { return "index"; }
 
     @GetMapping("/files")
-        List<Path> allFiles() {
+    @ResponseBody
+    List<Path> allFiles() {
         return fs.loadPaths().collect(Collectors.toList());
     }
 
--- src/main/java/com/homecloud/files/FilesStorageProperties.java
+++ src/main/java/com/homecloud/files/FilesStorageProperties.java
@@ -2,9 +2,6 @@ package com.homecloud.files;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 @ConfigurationProperties("filestorage")
 public class FilesStorageProperties {
 
