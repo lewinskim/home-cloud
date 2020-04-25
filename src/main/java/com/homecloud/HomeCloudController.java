53d005fb5f07ef421200acef1ccebdfa469b2910
Krzysiek <klopr@interia.pl>
2020-04-25 23:44:53 +0200

    Added basic remove and add direction/file

--- src/main/java/com/homecloud/HomeCloudController.java
+++ src/main/java/com/homecloud/HomeCloudController.java
@@ -4,8 +4,10 @@ import com.homecloud.files.FilesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.File;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -25,4 +27,20 @@ public class HomeCloudController {
         return fs.loadPaths().collect(Collectors.toList());
     }
 
+    @GetMapping("/remove")
+    @ResponseBody
+    boolean delete(@RequestParam(value = "file") String file){
+        String replace = file.replace("file:///", "");
+        File file1 = new File(replace);
+        return file1.delete();
+    }
+
+    @GetMapping("/add")
+    @ResponseBody
+    boolean create(@RequestParam(value = "file") String file,
+                   @RequestParam(value = "newdir") String dir){
+        String replace = file.replace("file:///", "");
+        File file1 = new File(replace + dir);
+        return file1.mkdir();
+    }
 }
